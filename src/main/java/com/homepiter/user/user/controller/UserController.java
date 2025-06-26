9bbaf7c244b1abeeb9e89fc7d6d470eaf970fb7c
Choiinhwan11 <Choiinhwan11>
2025-06-26 15:26:24 +0900

    gradle add - jpa ,jwt ,

--- src/main/java/com/homepiter/user/user/controller/UserController.java
+++ src/main/java/com/homepiter/user/user/controller/UserController.java
@@ -1,4 +1,9 @@
 package com.homepiter.user.user.controller;
 
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 }
